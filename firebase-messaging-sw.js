58bfc8bbc6fc8209161b835a7c7e6e50afe3e6bb
Hossam Galal <69807707+HossamHR@users.noreply.github.com>
2021-05-26 13:44:29 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,16 +1,16 @@
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js');
 
 var firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_FIREBASE_DOMAIN_NAME",
-    databaseURL: "YOUR_FIREBASE_DATBASE_URL",
-    projectId: "YOUR_FIREBASE_PROJECT_ID",
-    storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET END WITH appspot.com",
-    messagingSenderId: "YOUR SENDER ID",
-    appId: "YOUR APP ID",
-    measurementId: "YOUR MEASUREMENT ID"
-};
+    apiKey: "AIzaSyDw4sHWyPhkEGhniDh9S-zDyNljJE2ZAFc",
+    authDomain: "nonrox-2.firebaseapp.com",
+    databaseURL: "https://nonrox-2-default-rtdb.firebaseio.com",
+    projectId: "nonrox-2",
+    storageBucket: "nonrox-2.appspot.com",
+    messagingSenderId: "944147559503",
+    appId: "1:944147559503:web:e7528bb29d90068e0f263a",
+    measurementId: "G-09QGQXK7P5"
+  };
 
 firebase.initializeApp(firebaseConfig);
 const messaging=firebase.messaging();
